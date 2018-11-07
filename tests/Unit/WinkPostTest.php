2f40ecb15d219ba294341beb8276d05b0d40b988
Saleem Hadad <saleem_ha@hotmail.com>
2018-11-07 23:25:48 +0800

    add tests

--- tests/Unit/WinkPostTest.php
+++ tests/Unit/WinkPostTest.php
@@ -14,4 +14,30 @@ class WinkPostTest extends TestCase
 
         $this->assertEquals('custom title', $winkPost->title);
     }
+
+    /** @test */
+    public function it_has_unique_slug()
+    {
+        $winkPost = factory(WinkPost::class)->create(['slug' => 'custom-slug']);
+        $this->assertEquals('custom-slug', $winkPost->slug);
+
+        $this->expectException('\Illuminate\Database\QueryException');
+        factory(WinkPost::class)->create(['slug' => 'custom-slug']);
+    }
+
+    /** @test */
+    public function it_has_excerpt()
+    {
+        $winkPost = factory(WinkPost::class)->create(['excerpt' => 'custom excerpt']);
+
+        $this->assertEquals('custom excerpt', $winkPost->excerpt);
+    }
+
+    /** @test */
+    public function it_has_body()
+    {
+        $winkPost = factory(WinkPost::class)->create(['body' => 'custom body']);
+
+        $this->assertEquals('custom body', $winkPost->body);
+    }
 }
