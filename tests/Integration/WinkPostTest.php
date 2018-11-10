c671f5b9460fb10a3c377a9dc523eb945854dde0
Saleem Hadad <saleem_ha@hotmail.com>
2018-11-10 15:29:14 +0800

    wip

--- tests/Integration/WinkPostTest.php
+++ tests/Integration/WinkPostTest.php
@@ -2,9 +2,9 @@
 
 namespace Wink\Tests\Integration;
 
+use Wink\WinkPage;
 use Wink\WinkPost;
 use Wink\Tests\TestCase;
-use Wink\WinkPage;
 
 class WinkPostTest extends TestCase
 {
@@ -24,7 +24,7 @@ class WinkPostTest extends TestCase
 
         $winkPostData = $this->winkPostData(['author_id' => $user->id]);
 
-        // the id should be `new` in order to create new post.
+        // The id should be `new` in order to create new post.
         // otherwise will be considered as updating the post.
         $this->json('POST', route('wink.posts.store', 'new'), $winkPostData)->assertStatus(200);
 
@@ -38,13 +38,13 @@ class WinkPostTest extends TestCase
 
         $winkPostData = $this->winkPostData(['author_id' => $user->id]);
 
-        // When a user hit edit post that not exist => throw 404
+        // When a user hits edit post that not exist => throw 404
         $this->json('POST', route('wink.posts.store', 'not-exist-post-id'), $winkPostData)->assertStatus(404);
 
-        // however, if we have a post
+        // However, if we have a post
         $oldPost = factory(WinkPost::class)->create();
 
-        //the user now can edit it usign its id.
+        // The user now can edit it usign its id.
         $this->json('POST', route('wink.posts.store', $oldPost->id), $winkPostData)->assertStatus(200);
 
         $this->assertDatabaseHas('wink_posts', $winkPostData);
