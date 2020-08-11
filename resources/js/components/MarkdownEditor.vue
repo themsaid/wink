60715c0049a504765d7ed240426bbada12c257d3
Mohamed Said <themohamedsaid@gmail.com>
2020-08-11 06:47:30 +0200

    compile

--- resources/js/components/MarkdownEditor.vue
+++ resources/js/components/MarkdownEditor.vue
@@ -1,130 +1,134 @@
 <script type="text/ecmascript">
-import _ from "lodash";
+    import _ from "lodash";
 
-export default {
-    props: {
-        value: {
-            type: String,
-            default: ""
-        }
-    },
+    export default {
+        props: {
+            value: {
+                type: String,
+                default: ""
+            }
+        },
 
-    data() {
-        return {
-            textarea: null,
-            content: "",
-            uploadProgress: 0,
-            uploading: false
-        };
-    },
+        data() {
+            return {
+                textarea: null,
+                content: "",
+                uploadProgress: 0,
+                uploading: false
+            };
+        },
 
-    watch: {
-        content(val) {
-            this.$emit("input", val);
-        }
-    },
+        watch: {
+            content(val) {
+                this.$emit("input", val);
+            }
+        },
 
-    mounted() {
-        this.content = this.value;
+        mounted() {
+            this.content = this.value;
 
-        this.$nextTick(() => {
-            this.textarea = document.getElementById("markdown-editor");
-            var heightLimit = 2000;
+            this.$nextTick(() => {
+                this.textarea = document.getElementById("markdown-editor");
+                var heightLimit = 2000;
 
-            this.textarea.style.height =
-                Math.min(this.textarea.scrollHeight, heightLimit) + "px";
-
-            if (this.textarea) {
                 this.textarea.style.height =
                     Math.min(this.textarea.scrollHeight, heightLimit) + "px";
 
-                this.textarea.oninput = () => {
-                    this.textarea.style.height = "";
+                if (this.textarea) {
                     this.textarea.style.height =
-                        Math.min(this.textarea.scrollHeight, heightLimit) +
-                        "px";
-                };
+                        Math.min(this.textarea.scrollHeight, heightLimit) + "px";
 
-                this.textarea.addEventListener("paste", this.onPaste);
+                    this.textarea.oninput = () => {
+                        this.textarea.style.height = "";
+                        this.textarea.style.height =
+                            Math.min(this.textarea.scrollHeight, heightLimit) +
+                            "px";
+                    };
+
+                    this.textarea.addEventListener("paste", this.onPaste);
+                }
+            });
+        },
+
+        destroyed() {
+            if (this.textarea) {
+                this.textarea.removeEventListener("paste", this.onPaste);
             }
-        });
-    },
+        },
 
-    destroyed() {
-        if (this.textarea) {
-            this.textarea.removeEventListener("paste", this.onPaste);
-        }
-    },
-
-    methods: {
-        onPaste(e) {
-            if (e.clipboardData && e.clipboardData.items) {
-                var items = e.clipboardData.items;
-                for (var i = 0; i < items.length; i++) {
-                    if (items[i].type.indexOf("image") !== -1) {
-                        const image = items[i].getAsFile();
-                        this.uploadPastedImage(image);
-                        e.preventDefault();
+        methods: {
+            onPaste(e) {
+                if (e.clipboardData && e.clipboardData.items) {
+                    var items = e.clipboardData.items;
+                    for (var i = 0; i < items.length; i++) {
+                        if (items[i].type.indexOf("image") !== -1) {
+                            const image = items[i].getAsFile();
+                            this.uploadPastedImage(image);
+                            e.preventDefault();
+                        }
                     }
                 }
+            },
+
+            uploadPastedImage(image) {
+                let formData = new FormData();
+
+                formData.append("image", image, image.name);
+
+                const placeholder = `![Uploading ${image.name}…]()`;
+
+                this.insertAtCursor(placeholder);
+
+                this.http()
+                    .post("/api/uploads", formData, {
+                        onUploadProgress: progressEvent => {
+                            this.uploadProgress = Math.round(
+                                (progressEvent.loaded * 100) / progressEvent.total
+                            );
+                        }
+                    })
+                    .then(({data}) => {
+                        this.insertImage(placeholder, data.url);
+                    })
+                    .catch(error => {
+                        console.log(error);
+                    });
+            },
+
+
+            insertAtCursor(text) {
+                var isSuccessful;
+                this.textarea.focus();
+
+                isSuccessful = document.execCommand(
+                    "insertText",
+                    false,
+                    text
+                );
+
+                // Firefox (non-standard method)
+                if (!isSuccessful && typeof input.setRangeText === "function") {
+                    const start = input.selectionStart;
+                    input.setRangeText(text);
+                    // update cursor to be at the end of insertion
+                    input.selectionStart = input.selectionEnd = start + text.length;
+
+                    // Notify any possible listeners of the change
+                    const e = document.createEvent("UIEvent");
+                    e.initEvent("input", true, false);
+                    input.dispatchEvent(e);
+                }
+            },
+
+
+            insertImage(placeholder, url) {
+                this.content = this.content.replace(
+                    placeholder,
+                    `![image](${url} "image")`
+                );
             }
-        },
-
-        uploadPastedImage(image) {
-            let formData = new FormData();
-
-            formData.append("image", image, image.name);
-
-            const placeholder = `![Uploading ${image.name}…]()`;
-
-            this.insertAtCursor(placeholder);
-
-            this.http()
-                .post("/api/uploads", formData, {
-                    onUploadProgress: progressEvent => {
-                        this.uploadProgress = Math.round(
-                            (progressEvent.loaded * 100) / progressEvent.total
-                        );
-                    }
-                })
-                .then(({ data }) => {
-                    this.insertImage(placeholder, data.url);
-                })
-                .catch(error => {
-                    console.log(error);
-                });
-        },
-
-        insertAtCursor(text) {
-            this.textarea.focus();
-
-            const isSuccessful = document.execCommand(
-                "insertText",
-                false,
-                text
-            );
-
-            // Firefox (non-standard method)
-            if (!isSuccessful && typeof input.setRangeText === "function") {
-                const start = input.selectionStart;
-                input.setRangeText(text);
-                // update cursor to be at the end of insertion
-                input.selectionStart = input.selectionEnd = start + text.length;
-
-                // Notify any possible listeners of the change
-                const e = document.createEvent("UIEvent");
-                e.initEvent("input", true, false);
-                input.dispatchEvent(e);
-            }
-        },
-        insertImage(placeholder, url) {
-            this.content = this.content.replace(
-                placeholder,
-                `![alt text](${url} "alt text)`
-            );
         }
-    }
-};
+    };
 </script>
 
 <template>
