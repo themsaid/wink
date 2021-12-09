3bbd4e0c9370361b8e218000f3041a31bef89c48
Jerson Carin <63774442+jersoncarin@users.noreply.github.com>
2021-12-10 02:09:25 +0800

    Fix Invalid date on timestamps attribute

--- src/AbstractWinkModel.php
+++ src/AbstractWinkModel.php
@@ -3,6 +3,7 @@
 namespace Wink;
 
 use Illuminate\Database\Eloquent\Model;
+use DateTimeInterface;
 
 abstract class AbstractWinkModel extends Model
 {
@@ -14,5 +15,16 @@ abstract class AbstractWinkModel extends Model
     public function getConnectionName()
     {
         return config('wink.database_connection');
+    }
+    
+    /**
+     * Prepare a date for array / JSON serialization.
+     *
+     * @param \DateTimeInterface $date
+     * @return string
+     */
+    protected function serializeDate(DateTimeInterface $date)
+    {
+        return $date->format('Y-m-d H:i:s');
     }
 }
