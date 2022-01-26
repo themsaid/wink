69ceb53970b11e45dc82a0d8e8a02bdb7ce41687
Keoghan Litchfield <keoghan@klever.co.uk>
2022-01-26 17:30:31 +0000

    It's wink logout not logout_path now

--- src/Wink.php
+++ src/Wink.php
@@ -14,7 +14,7 @@ class Wink
         return [
             'unsplash_key' => config('services.unsplash.key'),
             'path' => config('wink.path'),
-            'logout_path' => route(config('wink.authentication.logout_route_name')),
+            'logout' => route(config('wink.authentication.logout_route_name')),
             'preview_path' => config('wink.preview_path'),
             'author' => auth('wink')->check() ? auth('wink')->user()->only('name', 'avatar', 'id') : null,
             'default_editor' => config('wink.editor.default'),
